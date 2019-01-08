62db010460d07d54601be561d6169222a41ea948
victorschreuder <45063011+victorschreuder@users.noreply.github.com>
2019-01-08 23:18:41 +0100

    spelers in canvas en kleur veranderen
    - er zit iets in de treinen waardoor je de kleur niet kan veranderen, als je het tesst op het vierkante 3d blok en er op klkt werkt het namelijk wel.
    - spelers bijgevoegd in canvas (moet nog veranderen van kleur als je aan de beurt bent)

---
+++ Ticket to ride/Assets/scripts/Tekstje.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tekstje : MonoBehaviour
+{
+      float tijd = 5f; //Seconds to read the text
+
+    void Start()
+    {
+        Invoke("Hide", tijd);
+              }
+
+    void Hide()
+    {
+        Destroy(gameObject);
+    }
+    
+ }
+
---
+++ Ticket to ride/Assets/scripts/VeranderKleur.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VeranderKleur : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        // zet een nieuwe kleur op dak van de trein
+        
+        Renderer rend = GetComponent<Renderer>();
+
+      
+        rend.material.shader = Shader.Find("_Color");
+        rend.material.SetColor("_Color", Color.green);
+
+       
+        rend.material.shader = Shader.Find("Specular");
+        rend.material.SetColor("_SpecColor", Color.red);
+
+    }
+}
