33940135cea99420f2d720723b106fc434f5a6f1
victorschreuder <45063011+victorschreuder@users.noreply.github.com>
2019-01-09 01:56:53 +0100

    kleur kan veranderen
    - De prefab is verandert, komt een 3d cube bovenop. Anders pakt het script hem niet. Als je op het dak (3d cube) klikt wordt het nu groen. Dit kan je in het script nog aanpassen dat het de kleur van alle spelers kan aannemen. De nieuwe prefab die gebruikt moet worden heet TreinGoed, alleen deze werkt!!

--- Ticket to ride/Assets/scripts/VeranderKleur.cs
+++ Ticket to ride/Assets/scripts/VeranderKleur.cs
@@ -7,14 +7,14 @@ public class VeranderKleur : MonoBehaviour
     private void OnMouseDown()
     {
         // zet een nieuwe kleur op dak van de trein
-        
+
         Renderer rend = GetComponent<Renderer>();
 
-      
+
         rend.material.shader = Shader.Find("_Color");
         rend.material.SetColor("_Color", Color.green);
 
-       
+
         rend.material.shader = Shader.Find("Specular");
         rend.material.SetColor("_SpecColor", Color.red);
 
