2d2bb0fd6b337eef2a36aebe9281a5de80215f94
Anica Kijewska <anica.kijewska@gmail.com>
2019-01-09 22:32:36 +0100

    proberen beurten te maken
    mijn laptop kan unity echt niet aan dus dit gaat wss nog wel even duren

---
+++ Ticket to ride/Assets/beurtscript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpelerScript : MonoBehaviour
+{
+	public float time = 40; //secondes dat iemand de beurt //heeft
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Hide()
+    {
+        
+    }
+}
