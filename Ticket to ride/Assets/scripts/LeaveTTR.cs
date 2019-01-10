a768d599ae5203816ac70484032e7e7663e977f6
victorschreuder <45063011+victorschreuder@users.noreply.github.com>
2019-01-10 01:07:37 +0100

    hoofdmenu knop

---
+++ Ticket to ride/Assets/scripts/LeaveTTR.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeaveTTR : MonoBehaviour
+{
+
+    public void Terug()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+}
